6c3b2d7ad0d5db8ffe902894a26e58247c982e82
itemic <terrankroft@gmail.com>
2018-10-27 21:55:08 +0800

    More adjectives git push '
    eq
    q

--- js/wsc.js
+++ js/wsc.js
@@ -7,7 +7,7 @@
 const generate = () => {
 	const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"] // yea it's not the letter but the sound but close enough
 	const articles = ["A", "The"]
-	const formats1 = ["Defunct", "Great","Big", "Unbound", "Transformed", "Unlikely", "Divided", "Entangled", "Imperfect", "Perfect", "Radical", "Mysterious", "Bound", "Closed", "Outside"]
+	const formats1 = ["Defunct", "Great","Big", "Unbound", "Transformed", "Unlikely", "Divided", "Entangled", "Imperfect", "Perfect", "Radical", "Mysterious", "Bound", "Closed", "Outside", "Open", "Tiny", "Inside", "Really Really Cool"]
 	const formats2 = ["Unbound", "Transformed", "Within", "Unbound", "Without", "in Motion", "in Flux", "in Love", "in Me", "in You", "Away", "on Fire", "Below", "Above", "Encircled", "Enlightened", "United", "Abroad", "Undefined"]
 
 	const article = articles[Math.floor(Math.random() * articles.length)]
